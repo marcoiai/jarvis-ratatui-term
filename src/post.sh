6d7cb6e51b6b0ccf48bb598bacf960eb8af2f934
Marco Simao <marco.a.simao@gmail.com>
2025-05-12 01:02:07 -0300

    Update post.sh

--- src/post.sh
+++ src/post.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CURL="curl -X POST \
-  'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBuoy2mFW-4AI6n3JPDNm-H1eijd5p0EZ8' \
+  'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=API_KEY' \
   --header 'Accept: */*' \
   --header 'User-Agent: Thunder Client (https://www.thunderclient.com)' \
   --header 'Content-Type: application/json' \
